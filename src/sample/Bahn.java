c4786671130765c3d0c4326be59aeb2dcf43fc36
Svenja <jumpy15@gmx.de>
2019-06-18 18:16:12 +0200

    Keine Ball Klasse mehr

---
+++ src/sample/Bahn.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Bahn {
+    public static double minX;
+    public static double maxX;
+    public static double minY;
+    public static double maxY;
+    public static double minZ;
+    public static double maxZ;
+
+    public Bahn(double minX_, double maxX_, double minY_, double maxY_, double minZ_, double maxZ_){
+        this.minX = minX_;
+        this.maxX = maxX_;
+        this.minY = minY_;
+        this.maxY = maxY_;
+        this.minZ = minZ_;
+        this.maxZ = maxZ_;
+    }
+}
--- src/sample/Mover.java
+++ src/sample/Mover.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
 public class Mover {
@@ -16,6 +17,9 @@ public class Mover {
     PVector location;
     PVector velocity;
     PVector acceleration;
+
+    Bahn element1;
+
     double mass;
     int radius = 16;
     public static Pane container = new Pane();
@@ -34,7 +38,7 @@ public class Mover {
         location = new PVector(width - radius,height - radius-1);
         velocity = new PVector(0,0);
         acceleration = new PVector(-0.001,0.01);
-        mass = 20;
+        mass = 5;
     }
 
     public void applyForce(PVector force){
@@ -44,6 +48,8 @@ public class Mover {
     }
 
     public void draw(){
+        element1 = new Bahn(0,20,0,40,0,10);
+
         pane1.setPrefSize(width,height);
         pane1.setLayoutX(800);
         Rectangle rectangle1 = new Rectangle(width,height, Color.WHITE);
@@ -51,8 +57,9 @@ public class Mover {
         pane1.getChildren().addAll(rectangle1);
 
         pane2.setPrefSize(width, height);
-        pane2.setLayoutX(600);
-        pane2.setLayoutY(200);
+        pane2.setLayoutX(650);
+        pane2.setLayoutY(150);
+        pane2.getTransforms().add(new Rotate(-45,0,0));
         Rectangle rectangle2 = new Rectangle(width,height, Color.RED);
         rectangle2.setStroke(Color.GRAY);
         pane2.getChildren().addAll(rectangle2);
@@ -77,7 +84,7 @@ public class Mover {
                 System.out.println("Location: (" + location.x + "; " + location.y + ")");
                 System.out.println("Velocity: (" + velocity.x + "; " + velocity.y + ")");
                 System.out.println("Acceleration: (" + acceleration.x + "; " + acceleration.y + ")");
-                System.out.println("Bounds: Pane = (" + width + "; " + height + ")");
+                System.out.println("Bounds: Bahn = (" + width + "; " + height + ")");
                 System.out.println("Bounds: Pane2 = (" + pane2.getLayoutX() + "; " + pane2.getLayoutY() + ")" + " || Container = (" + container.getBoundsInLocal().getMaxX() + ", " + container.getBoundsInLocal().getMaxY() + ")");
 
                 checkEdges();
@@ -96,7 +103,7 @@ public class Mover {
         } else if(location.x < radius){
             //velocity.x *= -1;
             //location.x = radius;
-            acceleration.y *= 2;
+            //acceleration.y *= 2;
             location.x = pane2.getLayoutX()+radius;
             pane2.getChildren().add(ball);
         }
